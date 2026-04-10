9161cb510fb128f771932dd4e27bcab45779ff3a
victorgyokeres <itsaadityasamant@gmail.com>
2026-04-10 17:03:44 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'buddy-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.hostname !== self.location.hostname) {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return response;
+      });
+    })
+  );
+});
